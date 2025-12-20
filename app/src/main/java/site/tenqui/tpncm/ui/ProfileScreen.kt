b119cbf2eba77bb55f933d0c2ff05eb4b811e6e6
尘柒 <109974035+Tenqui-Noir@users.noreply.github.com>
2025-12-20 11:27:58 +0800

    优化 Home 页列表实现

--- app/src/main/java/site/tenqui/tpncm/ui/ProfileScreen.kt
+++ app/src/main/java/site/tenqui/tpncm/ui/ProfileScreen.kt
@@ -1,18 +1,28 @@
 package site.tenqui.tpncm.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+
 @Composable
-fun ProfilesScreen() {
+fun ProfilesScreen(padding: PaddingValues) {
     Column(
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier
+            .padding(padding)
+            .padding(16.dp)
     ) {
-        Text("搜索")
-        Text("这里将来是搜索页面")
+        Text("我的")
+        Spacer(modifier = Modifier.height(12.dp))
+        Text("BatterNetEaseCloudMusicClient")
+        Text("Ver: 0.1.0")
+        Spacer(modifier = Modifier.height(12.dp))
+        Text("杂七杂八")
     }
 }
