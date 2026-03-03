3e11e4ddcfdc5a0b01122586b4fe5a5115831a29
尘柒 <109974035+Tenqui-Noir@users.noreply.github.com>
2026-03-03 16:35:39 +0800

    Initialize ProfileScreen.kt with the latest stable version

---
+++ app/src/main/java/site/tenqui/tpncm/ui/ProfileScreen.kt
@@ -0,0 +1,28 @@
+package site.tenqui.tpncm.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun ProfilesScreen(padding: PaddingValues) {
+    Column(
+        modifier = Modifier
+            .padding(padding)
+            .padding(16.dp)
+    ) {
+        Text("我的")
+        Spacer(modifier = Modifier.height(12.dp))
+        Text("Flowtone")
+        Text("Ver: 0.1.0")
+        Spacer(modifier = Modifier.height(12.dp))
+        Text("杂七杂八")
+    }
+}
